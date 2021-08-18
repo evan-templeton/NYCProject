449a6087332c0557313b2bae463a42ce859fdc2d
Evan Templeton <temevan@amazon.com>
2021-08-18 18:10:07 -0500

    Initial Commit

---
+++ 20210818-EvanTempleton-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210818-EvanTempleton-NYCSchools
+//
+//  Created by Templeton, Evan on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
